c05ed5a8992ee84bbc32e915a5e4fb6c2db2e8e9
Zohaib Rashid <47440269+ZohaibRashid@users.noreply.github.com>
2022-07-02 06:09:08 -0400

    add items to the list There are two state variables: newItem stores value of text user types in the box, upon button click, addItems() function is called which accesses the previous items and appends to the listItems array, the other state variable.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [listItems, setListItems] = useState([]);
+
+  function handleChange(event) {
+    const val = event.target.value;
+    setNewItem(val);
+  }
+
+  function addItems() {
+    setListItems(function (prevValue) {
+      return [...prevValue, newItem];
+    });
+    setNewItem("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" onChange={handleChange} value={newItem} />
+        <button onClick={addItems}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {listItems.map(function (item) {
+            return <li>{item}</li>;
+          })}
         </ul>
       </div>
     </div>
